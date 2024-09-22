e6262a247191e70e41dd456fecbb8676ee01069a
Josh Minnich <joshminnich@gmail.com>
2024-09-22 17:01:49 -0500

    chore: Complete chapter 17

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 import CardWrapper from '@/app/ui/dashboard/cards';
@@ -11,6 +12,10 @@ import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   return (
